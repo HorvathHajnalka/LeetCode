07d2c8d301ad9761485b8f23d189c45bf0f96459
Horvath Hajnalka <h.hajni2003@gmail.com>
2024-09-06 20:26:00 +0200

    product-of-array-except-self

--- coding-practice/product-of-array-except-self.py
+++ coding-practice/product-of-array-except-self.py
@@ -1,23 +1,16 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        answer = nums
-        multiplication_all = 1
-        zeros = 0
-        for j in range(0, len(nums)):
-            if nums[j] != 0:
-                multiplication_all *= nums[j]
-            else:
-                zeros += 1
-        for i in range(0, len(answer)):
-            if answer[i] != 0 and zeros == 0:
-                answer[i] = multiplication_all/answer[i]
-            elif answer[i] == 0 and zeros == 1:
-                answer[i] = multiplication_all
-            else:
-                answer[i] = 0
-            
-        return answer
+        output = [ 1 for i in range(len(nums))]
+        prod = 1
+
+        for i in range(len(nums) - 1):
+            prod *= nums[i]
+            output[i + 1] = prod
+            print(prod)
+
+        prod = 1
+        for j in range(len(nums) -1, 0, -1):
+            prod *= nums[j]
+            output[j - 1] *= prod
+
+        return output
