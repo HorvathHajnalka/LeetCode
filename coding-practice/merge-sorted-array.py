e75301bcf56840e286d2d00310f6507681a48e4a
Horvath Hajnalka <h.hajni2003@gmail.com>
2024-09-24 14:26:29 +0200

    merge-sorted-array

--- coding-practice/merge-sorted-array.py
+++ coding-practice/merge-sorted-array.py
@@ -1,28 +1,15 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
+        i1 = m - 1
+        i2 = n - 1
+        curr = (m + n) - 1
 
-        if m == 0:
-            for idx in range(0, len(nums2)):
-                nums1[idx] = nums2[idx]
-
-        i =  m - 1 # 0
-        j = n - 1 # 0
-        k = n + m - 1 # 1
-        while k >= 0:
-            if  j >= 0 and (i < 0 or nums2[j] > nums1[i]):
-                    nums1[k] = nums2[j]
-                    j -= 1
-
-            elif i >= 0:
-                nums1[k] = nums1[i]
-                i -= 1
-            k -= 1
-            
-            
+        while i2 >= 0:
+            if i1 >= 0 and nums1[i1] >= nums2[i2]:
+                nums1[curr] = nums1[i1]
+                curr -= 1
+                i1 -= 1
+            else:
+                nums1[curr] = nums2[i2]
+                curr -= 1
+                i2 -= 1
